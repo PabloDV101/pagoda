3e96ec6efbc13c0b15f10d2e029693c1a6c95bfb
PabloDV101 <j.pablo.d.v09@gmail.com>
2026-03-17 21:44:01 -0600

    creacion proyecto

---
+++ pagoda/src/test/java/com/proyectoBD/pagoda/PagodaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyectoBD.pagoda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PagodaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
